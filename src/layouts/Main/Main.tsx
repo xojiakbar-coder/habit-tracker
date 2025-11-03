4610bdaf93b4d2ee6aeb20e1aa59150e2411d08f
Xojiakbar <mistercoderjs@gmail.com>
2025-11-03 19:23:45 +0500

    home page

--- src/layouts/Main/Main.tsx
+++ src/layouts/Main/Main.tsx
@@ -1,17 +1,19 @@
 import { Outlet } from "react-router-dom";
-import { Button } from "@/components/Button";
 import { Mode } from "@/layouts/Aspects/Mode";
-import { Text, AppShell } from "@mantine/core";
+import { Text, AppShell, Group } from "@mantine/core";
 
 import styles from "./Main.module.scss";
+import { Button } from "@/components/Button";
 
 const Main = () => {
   return (
     <AppShell padding="0" header={{ height: 60 }}>
       <AppShell.Header classNames={{ header: styles.header }}>
         <Text className={styles.title}>Habit Tracker</Text>
-        {/* <Button variant="success" content="Content" /> */}
-        <Mode />
+        <Group>
+          <Button variant="outline" content="Contact" />
+          <Mode />
+        </Group>
       </AppShell.Header>
 
       <AppShell.Main classNames={{ main: styles.main }}>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import { storage } from "@/core/services";
+import { Title } from "@mantine/core";
+import { useEffect } from "react";
+import { toast } from "sonner";
+
+import styles from "./Home.module.scss";
 
 const Home = () => {
+  const userName = storage.local.get("username");
+
+  useEffect(() => {
+    if (!userName) {
+      toast.error("Your username is undefined", {
+        position: "top-center",
+      });
+    }
+  }, [userName]);
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.container}>
+      <Title order={1} className={styles.userName}>
+        Welcome {userName || "🤷"}
+      </Title>
     </div>
   );
 };
